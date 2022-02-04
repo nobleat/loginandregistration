0b826c9e522e4ebb6d4fa0a3aedc71edd46a1d50
Angela Noble <anoble33015@outlook.com>
2022-02-03 21:05:11 -0500

    first commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,29 @@
+from flask_app import app
+from flask import render_template, redirect,session,request, flash
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/register', methods=['POST'])
+def register():
+    if User.validate(request.form) ==False:
+        return redirect('/')
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    data={
+        'first_name':request.form['first_name'],
+        'last_name':request.form['last_name'],
+        'email':request.form['email'],
+        'password':pw_hash
+        }
+    user_id=User.save(data)
+    session['logged_in_user']=user_id
+    return redirect ('/success')
+
+@app.route('/success')
+def create_user():
+    User.save(request.form)
+    return render_template('success.html')
---
+++ flask_app/models/user.py
@@ -0,0 +1,57 @@
+from flask_app import app
+from flask import flash
+import re
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+db="log_and_reg"
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class User:
+    def __init__(self,data):
+        self.id=data['id']
+        self.first_name=data['first_name']
+        self.last_name=data['last_name']
+        self.email=data['email']
+        self.password=data['password']
+        self.created_at=data['created_at']
+        self.updated_at=data['updated_at']
+
+    @staticmethod
+    def validate(data):
+        is_valid=True
+        if len(data['first_name'])<2:
+            flash("First name must be at least 2 characters.")
+            is_valid=False
+        if len(data['last_name']) <2:
+            flash("Last name must be at least 2 characters.")
+            is_valid=False
+        if not EMAIL_REGEX.match(data['email']):
+            flash('Email is not valid. Please try again. ')
+            is_valid=False
+        if User.get_by_email({'email':data['email']}):
+            flash('Email is already in use.')
+            is_valid=False
+        if len(data['password']) <8:
+            flash("Password does not meet the requirements. Please try again.")
+            is_valid=False
+        if data['password'] != data['confirm_password']:
+            flash('Passwords do not match. Please try again.')
+            is_valid=False
+        return is_valid
+
+    @classmethod
+    def get_by_email(cls, data):
+        query='Select * from users where email = %(email)s'
+        data=connectToMySQL(db).query_db(query,data)
+        if data== ():
+            return False
+        else:
+            return cls(data[0])
+
+
+    @classmethod
+    def save(cls, data):
+        query = 'INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);'
+        return connectToMySQL(db).query_db(query,data)
